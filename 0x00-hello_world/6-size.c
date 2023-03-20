f169336e661e90726f7f315eca37ce97bc06bb58
Nondumiso <tshidinondumiso425@gmail.com>
2023-03-19 23:32:30 -0700

    commit changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print out sizes of data types in C
- * code by bwave ict
+ * 
  * Return: 0
 */
 int main(void)
@@ -19,3 +19,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
+
