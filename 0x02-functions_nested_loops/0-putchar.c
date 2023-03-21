45bcc25f370a00e72bbc7b948401443cf44ff2fd
Nondumiso <tshidinondumiso425@gmail.com>
2023-03-21 10:06:20 -0700

    commit changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - print _putchar
  *Return: 0
@@ -6,6 +7,7 @@
 
 int main(void)
 {
+
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -16,4 +18,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
+
 }
